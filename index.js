ede2fea86df9d0fe7c90e1e0d6bae07aa1effe93
Sandeep H M <sandeemnss@gmail.com>
2022-12-09 19:01:32 +0530

    change to deploy

--- index.js
+++ index.js
@@ -13,10 +13,10 @@ dotenv.config();
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
-mongoose
-  .connect(process.env.MONGO_URL)
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
+// mongoose
+//   .connect(process.env.MONGO_URL)
+//   .then(console.log("Connected to MongoDB"))
+//   .catch((err) => console.log(err));
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -32,11 +32,28 @@ app.post("/api/upload", upload.single("file"), (req, res) => {
   res.status(200).json("File has been uploaded");
 });
 
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+//Routes go here
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 app.use("/api/categories", categoryRoute);
 
-app.listen(5000, () => {
-  console.log("Backend is running");
+// app.listen(5000, () => {
+//   console.log("Backend is running");
+// });
+
+connectDB().then(() => {
+  app.listen(5000, () => {
+    console.log("listening for requests");
+  });
 });
